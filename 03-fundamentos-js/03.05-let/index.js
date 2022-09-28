cc32ac56ca59b1be9c1155514c019f0f38f73f59
Renan Silva <renansilvaytB@gmail.com>
2022-09-28 15:04:03 -0300

    05 let ✅

---
+++ 03-fundamentos-js/03.05-let/index.js
@@ -0,0 +1,12 @@
+let nome = 'renan'
+nome = 'renan silva'
+
+console.log(nome)
+let age = 23
+if (age > 18) {
+  //let test = 32
+  console.log('Estou no escopo')
+}
+
+//console.log(test)
+// vai dar not defined

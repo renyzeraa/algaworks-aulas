fe16569874533ba24bd6010176ae5e0bc29bbb09
Renan Silva <renansilvaytB@gmail.com>
2022-09-29 10:38:03 -0300

    10 rest e destructuring ✅

---
+++ 05-destructuring/05.10-rest-e-destructuring/index.js
@@ -0,0 +1,6 @@
+const fruits = ['banana', 'morango', 'manga', 'kiwi', 'maracuja']
+
+const [primeiro, segundo] = fruits
+const otherItems = fruits.slice(2)
+
+const [first, second, ...restant] = fruits

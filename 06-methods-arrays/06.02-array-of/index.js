8367e98a8980c28ee6a4d1553af5e8b57824558a
Renan Silva <renansilvaytB@gmail.com>
2022-09-29 13:26:06 -0300

    02 array of ✅

---
+++ 06-methods-arrays/06.02-array-of/index.js
@@ -0,0 +1,12 @@
+// criar novos arrays
+
+const array = [1, 2, 3]
+
+const array1 = new Array(1, 2, 3)
+
+// assim criara um array com 6 espaços vazios
+const array2 = new Array(6)
+array2.forEach(() => console.log('a'))
+
+// assim cria um array com um valor
+const array3 = Array.of(6)

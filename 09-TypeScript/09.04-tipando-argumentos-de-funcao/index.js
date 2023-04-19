90fb29e99bfa94823e3409c21aaf39637484b865
renyzeraa <renansilvaytb@gmail.com>
2023-04-19 19:16:56 -0300

    04 - tipando argumentos de funcoes ✅

--- 09-TypeScript/09.04-tipando-argumentos-de-funcao/index.js
+++ 09-TypeScript/09.04-tipando-argumentos-de-funcao/index.js
@@ -1,7 +1,9 @@
 "use strict";
-function greet(name) {
-    console.log(`Hello, ${name}!`);
+// em uma funcao sempre definir o tipo, pois por padrao vem any, onde tem grande chance de causar problemas na aplicação
+function greet(name, age) {
+    // esse sinal de ? no age, permite vir vazio caso nao definido o parametro
+    // se acaso nao definisse o tipo, poderia dar erro, pois toUpperCase executa somente com strings
+    console.log(`Hello, ${name.toUpperCase()}!`);
 }
-greet('Daniel');
-greet(0);
-greet(true);
+greet('Renan Silva', 24);
+greet('Fulano');
--- 09-TypeScript/09.04-tipando-argumentos-de-funcao/index.ts
+++ 09-TypeScript/09.04-tipando-argumentos-de-funcao/index.ts
@@ -1,7 +1,9 @@
-function greet (name:string, age?:number) {
-  console.log(
-    `Hello, ${name.toUpperCase()}!`
-  )
+// em uma funcao sempre definir o tipo, pois por padrao vem any, onde tem grande chance de causar problemas na aplicação
+function greet(name: string, age?: number) {
+    // esse sinal de ? no age, permite vir vazio caso nao definido o parametro
+    // se acaso nao definisse o tipo, poderia dar erro, pois toUpperCase executa somente com strings
+    console.log(`Hello, ${name.toUpperCase()}!`)
 }
 
-greet('Daniel', 1)
+greet('Renan Silva', 24)
+greet('Fulano')

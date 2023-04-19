eda764ea2c75fad60290464394575e066ee0bc1e
renyzeraa <renansilvaytb@gmail.com>
2023-04-19 19:04:45 -0300

    02 - tipando variaveis

--- 09-TypeScript/09.02-tipando-variaveis/index.js
+++ 09-TypeScript/09.02-tipando-variaveis/index.js
@@ -1,8 +1,11 @@
 "use strict";
-const personName = 'Daniel';
-const personAge = 22;
+// apos o nome da constante definimos o tipo dela
+// tipos primitivos como string, number, boolean
+const personName = 'Renan';
+const personAge = 24;
 const personAlive = true;
+// podemos definir o valor que tera dentro de um array tambem
 const personContacts = [
     0,
-    'daniel.bonifacio@algaworks.com',
+    'daniel.bonifacio@algaworks.com'
 ];
--- 09-TypeScript/09.02-tipando-variaveis/index.ts
+++ 09-TypeScript/09.02-tipando-variaveis/index.ts
@@ -1,8 +1,11 @@
-const personName:string = 'Daniel'
-const personAge:number = 22
-const personAlive:boolean = true
+// apos o nome da constante definimos o tipo dela
+// tipos primitivos como string, number, boolean
+const personName: string = 'Renan'
+const personAge: number = 24
+const personAlive: boolean = true
 
-const personContacts:(string|number)[] = [
-  0,
-  'daniel.bonifacio@algaworks.com',
+// podemos definir o valor que tera dentro de um array tambem
+const personContacts: (string | number)[] = [
+    0,
+    'daniel.bonifacio@algaworks.com'
 ]

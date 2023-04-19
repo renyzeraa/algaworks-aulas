1a2c5d4a8ef07fb4f57b34b3faf68c0000efd56f
renyzeraa <renansilvaytb@gmail.com>
2023-04-19 19:10:23 -0300

    03 - type aliasing ✅

---
+++ 09-TypeScript/09.03-type-aliasing/index.js
@@ -0,0 +1,2 @@
+'use strict'
+const sex = undefined
--- 09-TypeScript/09.03-type-aliasing/index.ts
+++ 09-TypeScript/09.03-type-aliasing/index.ts
@@ -1,4 +1,6 @@
+// criando um alias, uma tipo encurtado que define o tipo que sera aquela variavel
 type Cpf = string | number
 type Sex = 'male' | 'female' | undefined
 
-const sex:Sex = undefined
+// no caso sex devera receber apenas o 3 valores acima
+const sex: Sex = undefined

09ea79a21ca59919abbe85640d6b6cecfe76957e
renyzeraa <renansilvaytb@gmail.com>
2023-04-19 20:02:29 -0300

    07 - interface ✅

---
+++ 09-TypeScript/09.07-interfaces/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const person = {
+    age: 24,
+    name: 'Renan',
+    sex: 'male'
+};
--- 09-TypeScript/09.07-interfaces/index.ts
+++ 09-TypeScript/09.07-interfaces/index.ts
@@ -1,14 +1,15 @@
 interface Animal {
-  sex: 'male' | 'female'
+    sex: 'male' | 'female'
 }
 
+// como o extends das Classes
 interface Human extends Animal {
-  name: string
-  age: number
+    name: string
+    age: number
 }
 
 const person: Human = {
-  age: 22,
-  name: 'Lucas',
-  sex: 'male'
+    age: 24,
+    name: 'Renan',
+    sex: 'male'
 }

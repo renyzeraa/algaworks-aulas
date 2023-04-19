942672e9cd07a423e257917fbd4247d9306ab4a8
renyzeraa <renansilvaytb@gmail.com>
2023-04-19 18:16:24 -0300

    01 - npm install typescript

--- 09-TypeScript/09.01-typeScript/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const me = {
-    name: 'Daniel',
-    age: 22
-};
-exports.default = me;
--- 09-TypeScript/09.01-typeScript/index.ts
+++
@@ -1,6 +0,0 @@
-const me = {
-  name: 'Daniel',
-  age: 22
-}
-
-export default me
